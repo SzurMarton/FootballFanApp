1b5c5dcb5eb9180403abeb565109640f2a9be22c
Márton Szűr <56449356+SzurMarton@users.noreply.github.com>
2021-02-24 21:23:07 +0100

    Gradel dependencies fix

--- app/src/main/java/com/footballfan/RegisterInteractor.kt
+++ app/src/main/java/com/footballfan/RegisterInteractor.kt
@@ -1,12 +1,11 @@
 package com.footballfan
 
+import com.google.firebase.auth.FirebaseAuth
 import javax.inject.Inject
 
 
 class RegisterInteractor @Inject constructor(){
     suspend fun saveUser(email : String, password: String) : Boolean{
-        //return FirebaseAuth.getInstance().createUserWithEmailAndPassword(email,password).isSuccessful
-        //FirebaseAuth.getIns
-        return true
+        return FirebaseAuth.getInstance().createUserWithEmailAndPassword(email,password).isSuccessful
     }
 }
