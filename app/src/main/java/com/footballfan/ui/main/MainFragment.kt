bbcef6447d44b259ba3025f6d65e24deca0a98a9
Márton Szűr <56449356+SzurMarton@users.noreply.github.com>
2021-03-02 19:38:07 +0100

    Add viewpager

--- app/src/main/java/com/footballfan/ui/main/MainFragment.kt
+++ app/src/main/java/com/footballfan/ui/main/MainFragment.kt
@@ -1,9 +1,17 @@
 package com.footballfan.ui.main
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.viewpager2.widget.ViewPager2
 import co.zsmb.rainbowcake.base.RainbowCakeFragment
 import co.zsmb.rainbowcake.dagger.getViewModelFromFactory
 import com.footballfan.R
 import com.footballfan.ui.main.ViewReady
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
+import kotlinx.android.synthetic.main.fragment_main.*
 
 class MainFragment : RainbowCakeFragment<MainViewState,MainViewModel>() {
     override fun provideViewModel(): MainViewModel = getViewModelFromFactory()
@@ -14,4 +22,24 @@ class MainFragment : RainbowCakeFragment<MainViewState,MainViewModel>() {
             ViewReady -> {}
         }
     }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? =
+            inflater.inflate(R.layout.fragment_blank, container, false)
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        initViewPager2WithFragments()
+    }
+
+    private fun initViewPager2WithFragments(){
+        viewpager.adapter = MainAdapter(childFragmentManager,lifecycle)
+
+        //var tabLayout:TabLayout = findViewById(R.id.tablayout)
+        var names:Array<String> = arrayOf("1","2","3")
+        TabLayoutMediator(tablayout,viewpager){
+            tab, position ->
+            tab.text = names[position]
+        }.attach()
+    }
+
 }
