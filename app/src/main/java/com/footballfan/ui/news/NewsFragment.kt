2866a2344b004e8c18ac0421951fd48ab3e01c7c
Márton Szűr <56449356+SzurMarton@users.noreply.github.com>
2021-03-03 17:26:21 +0100

    Add viewflipper

--- app/src/main/java/com/footballfan/ui/news/NewsFragment.kt
+++ app/src/main/java/com/footballfan/ui/news/NewsFragment.kt
@@ -2,8 +2,10 @@ package com.footballfan.ui.news
 
 import co.zsmb.rainbowcake.base.RainbowCakeFragment
 import co.zsmb.rainbowcake.dagger.getViewModelFromFactory
+import co.zsmb.rainbowcake.extensions.exhaustive
 import com.footballfan.R
 import com.footballfan.ui.main.ViewReady
+import kotlinx.android.synthetic.main.fragment_news.*
 
 class NewsFragment : RainbowCakeFragment<NewsViewState,NewsViewModel>() {
     override fun provideViewModel(): NewsViewModel = getViewModelFromFactory()
@@ -11,8 +13,10 @@ class NewsFragment : RainbowCakeFragment<NewsViewState,NewsViewModel>() {
 
     override fun render(viewState: NewsViewState) {
         when(viewState){
-            Loading -> {}
-        }
+            Loading -> {
+                viewFlipperMain.displayedChild = 0
+            }
+        }.exhaustive
     }
 
 }
